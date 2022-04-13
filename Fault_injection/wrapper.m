21b8d4abdbd413ff4d9f9d21f435945bc2af36a4
Vishnu_Naik <vishnugnaik@gmail.com>
2022-04-14 01:04:58 +0200

    Fault type output added to FIB Library

--- Fault_injection/wrapper.m
+++ Fault_injection/wrapper.m
@@ -1,27 +1,17 @@
-function [error_data, error_flag] = wrapper(data, time, name, flag)
-% % d = data;
-% % t = time;
-% % n = name;
-% % f = flag;
-%global f_flag;
-%f = f_flag;
+function [error_data, error_flag, fault_type] = wrapper(data, time, name, flag)
+
 error_data = data;
 error_flag = 0;
 global finjectors;
 ff = 2;
-% disp('Finit s')
-% disp(name)
-% disp(keys(finjectors))
-% disp('Finit e')
 try 
     ff = finjectors(name);
 catch
     ne = name;
     warning('NOT EXIST');
     warning(name);
-    %disp(n);
 end
-%disp(ff);
+
 if ff ~= 2
     if (ff.fexp_flag == 1)
         if (flag > 0 && ff.fail_trigger ~= 1)
@@ -32,6 +22,23 @@ if ff ~= 2
         end
         error_data = ff.finject(data, time);
         error_flag = ff.fail_flag;
+        ft = ff.fault_type;	
+        if error_flag == 0	
+            fault_type = 0;	
+        elseif (strcmp(ft, 'Sensor: Noise'))	
+            fault_type = 1;	
+        elseif (strcmp(ft, 'Sensor: Offset'))	
+            fault_type = 2;	
+        elseif (strcmp(ft, 'Hardware: Bit flips'))	
+            fault_type = 3;	
+        elseif (strcmp(ft, 'Sensor: Stuck-at fault'))	
+            fault_type = 4;	
+        elseif (strcmp(ft, 'Network: Time delay'))	
+            fault_type = 5;	
+        else	
+            fault_type = 6;	
+        end	
+        fault_type = convertStringsToChars(fault_type);
         %disp('injecting');
     end
 end
